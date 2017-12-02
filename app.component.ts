7f88809bb0652316f5ebd1e1c19047028f719264
Cab <cabalor@gmail.com>
2017-12-02 15:37:10 +0100

    ada

--- app.component.ts
+++ app.component.ts
@@ -1,3 +1,4 @@
+import { CorsesServ } from './courses.service';
 import { ClickChangeArgs } from './favor/favor.component';
 import { Component } from '@angular/core';
 
@@ -30,5 +31,19 @@ export class AppComponent {
     console.log("clicked ", eventArgs);
   }
 
+  viewSwitch = 'dupsko';
+
+  games = [{id:1, name:"dungeon crawler"}, {id:2, name:"star wras crawler"},
+{id:3, name:"arham crawler"}
+]
+
+addidtion(){
+  this.games.push({id:4, name:'ryzkanci'});
+}
+
+remGame(game){
+let int = this.games.indexOf(game);
+this.games.splice(int, 1);
+}
 
 }
