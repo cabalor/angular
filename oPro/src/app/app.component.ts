7c904aadc7ff15bccb1ee1e1655e77e6449e7a7e
Cab <cabalor@gmail.com>
2018-02-21 20:10:03 +0100

    Asdad

--- oPro/src/app/app.component.ts
+++ oPro/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth-service.service';
+import { Component, OnInit } from '@angular/core';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnDestroy{
+
+  
+  constructor(private auth: AuthService, router: Router){
+    }
+      
+    OnInit(auth, router){
+      auth.user$.subscribe(user => {
+        if(user){
+          let returnUrl = localStorage.getItem('returnUrl');
+          router.navigateByUrl(returnUrl);
+        }
+      });
+    }
+
+    ngOnDestroy(auth){
+      auth.user$.unsubscribe();
+    }
 }
--- oPro/src/app/auth-service.service.ts
+++ oPro/src/app/auth-service.service.ts
@@ -1,19 +1,24 @@
+import { ActivatedRoute } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase';
-
 @Injectable()
 export class AuthService {
-  //user$: Observable<firebase.User>;
-  constructor(private fireAuth: AngularFireAuth) { 
 
-    //this.user$ = fireAuth.authState;
+  user$: Observable<firebase.User>;
+  constructor(private fireAuth: AngularFireAuth, private route: ActivatedRoute) { 
+
+    this.user$ = fireAuth.authState;
   }
 
   login(){
+      let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+      localStorage.setItem('returnUrl', returnUrl);
       this.fireAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
   logout(){
+    this.fireAuth.auth.signOut();
   }
 }
