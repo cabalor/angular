48826a346421e6564e1529713c7ea08762496042
Cab <cabalor@gmail.com>
2018-03-28 21:31:53 +0200

    dodanie biblioteki

--- oPro/src/app/app.module.ts
+++ oPro/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CustomFormsModule } from 'ng2-validation';
+import { DataTableModule } from 'angular-4-data-table-aot';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
@@ -49,6 +50,7 @@ import { ProductService } from './product.service';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuth,
+    DataTableModule,
     NgbModule.forRoot(),
     RouterModule.forRoot([
       {path:'', component: HomeComponent},
